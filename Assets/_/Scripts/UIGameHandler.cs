bb2d75a1dbc3b24213e6cbd2cecfed805fe932ba
mraimbot <mraimbot@hotmail.de>
2021-11-18 22:16:46 +0100

    added score text

--- Assets/_/Scripts/UIGameHandler.cs
+++ Assets/_/Scripts/UIGameHandler.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _.Scripts
 {
     public class UIGameHandler : MonoBehaviour
     {
+        [SerializeField] private Text textScore;
+        
         public void OnBackToMenuClicked()
         {
             GameManager.LoadMenuScene();
+        }
+
+        public void UpdateScoreText(int newScore)
+        {
+            textScore.text = newScore.ToString();
         }
     }
 }
